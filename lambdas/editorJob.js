17934e48b15f161158f4b61e38b6332df5d1bf91
DivyanshuKaushik <divyanshukaushik33@gmail.com>
2022-01-22 19:33:21 +0530

    Updated EditorJob

--- lambdas/editorJob.js
+++ lambdas/editorJob.js
@@ -1,60 +1,59 @@
 const AWS = require('aws-sdk')
 const Response = require('./utils/Response')
-// const { deleteImage } = require('./utils/S3')
+const { deleteImage } = require('./utils/S3')
 const DB = new AWS.DynamoDB.DocumentClient()
 
 module.exports.handler = async event =>{
     try{
-        const TableName = process.env.TableName
+        // const TableName = process.env.TableName
 
-        if(event.path=="/getUnpublishedPosts"){
-            const params = {
-                TableName,
-                KeyConditionExpression: '#pk = :pk AND begins_with(sk,:starts)',
-                FilterExpression:"#status <> :stat",
-                ExpressionAttributeNames: {
-                    "#pk": "pk",
-                    "#status":"status"
-                },
-                ExpressionAttributeValues: {
-                    ':pk': "NewsBay_Article",
-                    ':starts': 'article_',
-                    ':stat':"published"
-                },
-                ScanIndexForward: false
-            }
-            const data = await DB.query(params).promise()
-            return Response(200,{articles:data.Items}) 
-        }
-        if(event.path=="/postStatus"){
+        // if(event.path=="/getUnpublishedPosts"){
+        //     const params = {
+        //         TableName,
+        //         KeyConditionExpression: '#pk = :pk AND begins_with(sk,:starts)',
+        //         FilterExpression:"#status <> :stat",
+        //         ExpressionAttributeNames: {
+        //             "#pk": "pk",
+        //             "#status":"status"
+        //         },
+        //         ExpressionAttributeValues: {
+        //             ':pk': "NewsBay_Article",
+        //             ':starts': 'article_',
+        //             ':stat':"published"
+        //         },
+        //         ScanIndexForward: false
+        //     }
+        //     const data = await DB.query(params).promise()
+        //     return Response(200,{articles:data.Items}) 
+        // }
+        // if(event.path=="/postStatus"){
 
-            let {sk,status} = JSON.parse(event.body)
-            const params = {
-                TableName,
-                Key:{
-                    pk:"NewsBay_Article",
-                    sk:sk
-                },
-                UpdateExpression: "set #stat = :status",
-                ExpressionAttributeNames:{
-                    "#stat":"status"
-                },
-                ExpressionAttributeValues:{
-                    ":status":status
-                },
-                ReturnValues:"UPDATED_NEW"
-            };
+        //     let {sk,status} = JSON.parse(event.body)
+        //     const params = {
+        //         TableName,
+        //         Key:{
+        //             pk:"NewsBay_Article",
+        //             sk:sk
+        //         },
+        //         UpdateExpression: "set #stat = :status",
+        //         ExpressionAttributeNames:{
+        //             "#stat":"status"
+        //         },
+        //         ExpressionAttributeValues:{
+        //             ":status":status
+        //         },
+        //         ReturnValues:"UPDATED_NEW"
+        //     };
            
-            const res = await DB.update(params).promise()
+        //     const res = await DB.update(params).promise()
            
-            return Response(200,{message:`Article ${status}!!`,res})
+        //     return Response(200,{message:`Article ${status}!!`,res})
 
-        }
+        // }
         // if(event.path==`/deletePost`){
         //     const {id,image} = event.queryStringParameters
         //     const img_name = image.split('.com/')[1]
-        //     // const delete_img = await deleteImage(img_name)
-        //     const delete_img = false
+        //     const delete_img = await deleteImage(img_name)
         //     if(!delete_img){
         //         return Response(400,{message:"Error Deleting Article!!"})
         //     }
@@ -68,7 +67,7 @@ module.exports.handler = async event =>{
         //     const res = await DB.delete(params).promise()
         //     return Response(200,{message:"Article deleted successfully!!"}) 
         // }
-
+        return Response(200,{message:"hello",event})
     }catch(error){
         return Response(400,{error})
     }
