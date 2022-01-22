6378efd65aa10c910c0fc374c29247cca5acf3a8
DivyanshuKaushik <divyanshukaushik33@gmail.com>
2022-01-22 19:44:43 +0530

    Fixed Path - Editor lambda

--- lambdas/editorJob.js
+++ lambdas/editorJob.js
@@ -5,69 +5,70 @@ const DB = new AWS.DynamoDB.DocumentClient()
 
 module.exports.handler = async event =>{
     try{
-        // const TableName = process.env.TableName
+        const TableName = process.env.TableName
+        const path = `/${event.path.split('/').reverse()[0]}`
 
-        // if(event.path=="/getUnpublishedPosts"){
-        //     const params = {
-        //         TableName,
-        //         KeyConditionExpression: '#pk = :pk AND begins_with(sk,:starts)',
-        //         FilterExpression:"#status <> :stat",
-        //         ExpressionAttributeNames: {
-        //             "#pk": "pk",
-        //             "#status":"status"
-        //         },
-        //         ExpressionAttributeValues: {
-        //             ':pk': "NewsBay_Article",
-        //             ':starts': 'article_',
-        //             ':stat':"published"
-        //         },
-        //         ScanIndexForward: false
-        //     }
-        //     const data = await DB.query(params).promise()
-        //     return Response(200,{articles:data.Items}) 
-        // }
-        // if(event.path=="/postStatus"){
+        if(path=="/getUnpublishedPosts"){
+            const params = {
+                TableName,
+                KeyConditionExpression: '#pk = :pk AND begins_with(sk,:starts)',
+                FilterExpression:"#status <> :stat",
+                ExpressionAttributeNames: {
+                    "#pk": "pk",
+                    "#status":"status"
+                },
+                ExpressionAttributeValues: {
+                    ':pk': "NewsBay_Article",
+                    ':starts': 'article_',
+                    ':stat':"published"
+                },
+                ScanIndexForward: false
+            }
+            const data = await DB.query(params).promise()
+            return Response(200,{articles:data.Items}) 
+        }
+        if(path=="/postStatus"){
 
-        //     let {sk,status} = JSON.parse(event.body)
-        //     const params = {
-        //         TableName,
-        //         Key:{
-        //             pk:"NewsBay_Article",
-        //             sk:sk
-        //         },
-        //         UpdateExpression: "set #stat = :status",
-        //         ExpressionAttributeNames:{
-        //             "#stat":"status"
-        //         },
-        //         ExpressionAttributeValues:{
-        //             ":status":status
-        //         },
-        //         ReturnValues:"UPDATED_NEW"
-        //     };
+            let {sk,status} = JSON.parse(event.body)
+            const params = {
+                TableName,
+                Key:{
+                    pk:"NewsBay_Article",
+                    sk:sk
+                },
+                UpdateExpression: "set #stat = :status",
+                ExpressionAttributeNames:{
+                    "#stat":"status"
+                },
+                ExpressionAttributeValues:{
+                    ":status":status
+                },
+                ReturnValues:"UPDATED_NEW"
+            };
            
-        //     const res = await DB.update(params).promise()
+            const res = await DB.update(params).promise()
            
-        //     return Response(200,{message:`Article ${status}!!`,res})
+            return Response(200,{message:`Article ${status}!!`,res})
 
-        // }
-        // if(event.path==`/deletePost`){
-        //     const {id,image} = event.queryStringParameters
-        //     const img_name = image.split('.com/')[1]
-        //     const delete_img = await deleteImage(img_name)
-        //     if(!delete_img){
-        //         return Response(400,{message:"Error Deleting Article!!"})
-        //     }
-        //     const params = {
-        //         TableName,
-        //         Key:{
-        //             pk:"NewsBay_Article",
-        //             sk:id
-        //         }
-        //     }
-        //     const res = await DB.delete(params).promise()
-        //     return Response(200,{message:"Article deleted successfully!!"}) 
-        // }
-        return Response(200,{message:"hello",event})
+        }
+        if(path==`/deletePost`){
+            const {id,image} = event.queryStringParameters
+            const img_name = image.split('.com/')[1]
+            const delete_img = await deleteImage(img_name)
+            if(!delete_img){
+                return Response(400,{message:"Error Deleting Article!!"})
+            }
+            const params = {
+                TableName,
+                Key:{
+                    pk:"NewsBay_Article",
+                    sk:id
+                }
+            }
+            const res = await DB.delete(params).promise()
+            return Response(200,{message:"Article deleted successfully!!"}) 
+        }
+
     }catch(error){
         return Response(400,{error})
     }
