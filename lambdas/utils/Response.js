2e131a7c30002c0216e7a55467efaf240e86abc9
DivyanshuKaushik <divyanshukaushik33@gmail.com>
2022-02-01 19:07:08 +0530

    fixed cors header

--- lambdas/utils/Response.js
+++ lambdas/utils/Response.js
@@ -1,9 +1,10 @@
 const Response = (code,data)=>{
     return {
         headers: {
-                 'Content-Type': 'application/json',
                  'Access-Control-Allow-Methods':'*',
-                 'Access-Control-Allow-Origin':'*'
+                 'Access-Control-Allow-Origin':'*',
+                 "Access-Control-Allow-Headers":"*",
+                 'Access-Control-Allow-Credentials': true,
         },
         statusCode:code,
         body:JSON.stringify(data)
